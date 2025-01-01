ebb7a5e13af9cc6d3c35b7e073bdbc8546b7011f
Ahmed Osman <ahmed.fathy.dev@gmail.com>
2025-01-01 10:50:48 +0200

    Update 124. Binary Tree Maximum Path Sum

--- 124. Binary Tree Maximum Path Sum/Solution.cpp
+++ 124. Binary Tree Maximum Path Sum/Solution.cpp
@@ -4,22 +4,22 @@ class Solution
 {
 private:
     int max_path_sum;
-
     int maxPath(TreeNode *root)
     {
         if (!root)
             return -1000;
 
-        int root_left = maxPath(root->left);
-        int root_right = maxPath(root->right);
+        int left_nodes = maxPath(root->left);
+        int right_nodes = maxPath(root->right);
 
-        max_path_sum = MAX(max_path_sum, MAX(MAX(root->val, root->val + MAX(root_left, root_right)), root_left + root->val + root_right));
+        max_path_sum = MAX(max_path_sum, MAX(MAX(root->val, root->val + MAX(left_nodes, right_nodes)), left_nodes + root->val + right_nodes));
 
-        return MAX(root->val, root->val + MAX(root_left, root_right));
+        return MAX(root->val, root->val + MAX(left_nodes, right_nodes));
     }
 
 public:
     Solution() : max_path_sum(-1000) {}
+
     int maxPathSum(TreeNode *root)
     {
         maxPath(root);
